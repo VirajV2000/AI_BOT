132e5f19cae790a3aacc2295c64a5909d8af1d35
VirajV2000 <virajv2000.com>
2024-10-27 09:07:35 +0530

    first commit

---
+++ backend/src/index.ts
@@ -0,0 +1,16 @@
+import  express from 'express';
+const app = express()
+
+app.use(express.json())
+
+app.post('/users/:id',(req,res)=>{
+  console.log(req.params);
+  
+  res.json({"id":req.params.id,"name":req.body.name});
+})
+app.get('/', (req, res) => {
+  res.send('hello world')
+  console.log(req.body);
+})
+
+app.listen(3000,()=>console.log("Server started"));
